0a0c6083573f79ffe0d7c95617344c4467e54d31
Kamil Moszczyc <likeyouop@gmail.com>
2024-07-16 18:30:59 +0200

    Run chat ETL before turning on Ozjasz Bot.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from src.core.client_api_handler import ClientAPIHandler
 from src.core.ozjasz_bot import OzjaszBot
-import logging
+
+from src.stats.chat_etl import ChatETL
 
 if __name__ == '__main__':
+    # Before running the bot, update the data from the past 24h first.
+    api_handler = ClientAPIHandler()
+    chat_stats = ChatETL(api_handler)
+    chat_stats.update(1)
+
     OzjaszBot()
