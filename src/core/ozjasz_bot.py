156f4bcd2213df43032d127feb3fd1d35ebe11af
Kamil Moszczyc <likeyouop@gmail.com>
2024-09-03 19:28:48 +0200

    Just doin some clean up here.

--- src/core/ozjasz_bot.py
+++ src/core/ozjasz_bot.py
@@ -28,40 +28,10 @@ class OzjaszBot:
         self.application.run_polling()
 
     def add_commands(self):
-        # command_handlers = [CommandHandler('ozjasz', self.core_commands.cmd_ozjasz),
-        #                     CommandHandler('bartosiak', self.core_commands.cmd_bartosiak),
-        #                     CommandHandler('tvp', self.core_commands.cmd_tvp),
-        #                     CommandHandler('tvp_latest', self.core_commands.cmd_tvp_latest),
-        #                     CommandHandler('tusk', self.core_commands.cmd_tusk),
-        #                     CommandHandler('starababa', self.core_commands.cmd_are_you_lucky_today),
-        #                     CommandHandler('help', self.core_commands.cmd_help),
-        #                     CommandHandler('bible', lambda update, context: self.core_commands.cmd_bible(update, context, self.bot_state)),
-        #                     CommandHandler('handlowa', self.core_commands.cmd_show_shopping_sundays),
-        #                     CommandHandler('commands', self.chat_commands.cmd_command_usage),
-        #                     CommandHandler('summary', self.chat_commands.cmd_summary),
-        #                     CommandHandler('topmessages', lambda update, context: self.chat_commands.cmd_messages_by_reactions(update, context, EmojiType.ALL)),
-        #                     CommandHandler('sadmessages', lambda update, context: self.chat_commands.cmd_messages_by_reactions(update, context, EmojiType.NEGATIVE)),
-        #                     CommandHandler('topmemes', lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.IMAGE, EmojiType.ALL)),
-        #                     CommandHandler('sadmemes', lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.IMAGE, EmojiType.NEGATIVE)),
-        #                     CommandHandler('topvideos', lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.VIDEO, EmojiType.ALL)),
-        #                     CommandHandler('topgifs', lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.GIF, EmojiType.ALL)),
-        #                     CommandHandler('topaudio', lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.AUDIO, EmojiType.ALL)),
-        #                     CommandHandler('lastmessages', lambda update, context: self.chat_commands.cmd_last_messages(update, context)),
-        #                     CommandHandler('displayusers', lambda update, context: self.chat_commands.cmd_display_users(update, context)),
-        #                     CommandHandler('setusername', lambda update, context: self.chat_commands.cmd_set_username(update, context)),
-        #                     CommandHandler('addnickname', lambda update, context: self.chat_commands.cmd_add_nickname(update, context)),
-        #                     CommandHandler('fun', lambda update, context: self.chat_commands.cmd_fun(update, context)),
-        #                     CommandHandler('wholesome', lambda update, context: self.chat_commands.cmd_wholesome(update, context)),
-        #                     CommandHandler('funchart', lambda update, context: self.chat_commands.cmd_funchart(update, context)),
-        #                     CommandHandler('spamchart', lambda update, context: self.chat_commands.cmd_spamchart(update, context)),
-        #                     CommandHandler('likechart', lambda update, context: self.chat_commands.cmd_likechart(update, context))
-        #                     ]
-
         commands_map = self.get_commands_map()
-        decorated_commands_map = {command_name: self.command_logger.count_command(command_name)(func) for command_name, func in commands_map.items()} # Apply the command counter decorator
+        decorated_commands_map = {command_name: self.command_logger.count_command(command_name)(func) for command_name, func in commands_map.items()}  # Apply the command counter decorator
         command_handlers = [CommandHandler(command_name, func) for command_name, func in decorated_commands_map.items()]
         self.application.add_handlers(command_handlers)
-
 
     def get_commands_map(self):
         return {
@@ -83,8 +53,8 @@ class OzjaszBot:
             'topvideos': lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.VIDEO, EmojiType.ALL),
             'topgifs': lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.GIF, EmojiType.ALL),
             'topaudio': lambda update, context: self.chat_commands.cmd_media_by_reactions(update, context, MessageType.AUDIO, EmojiType.ALL),
-            'lastmessages':  self.chat_commands.cmd_last_messages,
-            'displayusers':self.chat_commands.cmd_display_users,
+            'lastmessages': self.chat_commands.cmd_last_messages,
+            'displayusers': self.chat_commands.cmd_display_users,
             'setusername': self.chat_commands.cmd_set_username,
             'addnickname': self.chat_commands.cmd_add_nickname,
             'fun': self.chat_commands.cmd_fun,
