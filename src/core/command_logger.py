f7b5acb6db376fa8d5e22c8653984681ba6d2ca5
Kamil Moszczyc <likeyouop@gmail.com>
2024-10-21 10:40:26 +0200

    Remove this weird dt fix for command logger that caused issues with schema validation.

--- src/core/command_logger.py
+++ src/core/command_logger.py
@@ -7,9 +7,9 @@ import pandas as pd
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from src.core.utils import datetime_to_ms, read_df, save_df
+from src.core.utils import read_df, save_df
 from src.models.command_args import CommandArgs
-from src.stats.utils import filter_by_time_df, username_to_user_id
+from src.stats.utils import filter_by_time_df
 from definitions import COMMANDS_USAGE_PATH, TIMEZONE
 
 
@@ -29,7 +29,7 @@ class CommandLogger:
                 user_id = update.effective_user.id
                 timestamp = datetime.now()
                 new_entry = pd.DataFrame([{'timestamp': timestamp, 'user_id': user_id, 'command_name': command_name}])
-                new_entry['timestamp'] = pd.to_datetime(new_entry['timestamp'], utc=True).astype('datetime64[ns]').dt.tz_convert(TIMEZONE)
+                new_entry['timestamp'] = pd.to_datetime(new_entry['timestamp'], utc=True).dt.tz_convert(TIMEZONE)
 
                 self.command_usage_df = pd.concat([self.command_usage_df, new_entry], ignore_index=True)
                 save_df(self.command_usage_df, COMMANDS_USAGE_PATH)
@@ -45,13 +45,13 @@ class CommandLogger:
         if command_df is None:
             command_df = pd.DataFrame(columns=['timestamp', 'user_id', 'command_name'])
 
-        command_df['timestamp'] = pd.to_datetime(command_df['timestamp'], utc=True).astype('datetime64[ns]').dt.tz_convert(TIMEZONE)
+        command_df['timestamp'] = pd.to_datetime(command_df['timestamp'], utc=True).dt.tz_convert(TIMEZONE)
         return command_df
 
     def preprocess_data(self, users_df, command_args: CommandArgs):
         filtered_df = filter_by_time_df(self.command_usage_df, command_args)
         filtered_df['username'] = filtered_df.merge(users_df[['final_username']], on='user_id', how='left')['final_username']
-        filtered_df['timestamp'] = pd.to_datetime(filtered_df['timestamp'], utc=True).astype('datetime64[ns]').dt.tz_convert(TIMEZONE)
+        filtered_df['timestamp'] = pd.to_datetime(filtered_df['timestamp'], utc=True).dt.tz_convert(TIMEZONE)
 
         if command_args.user is not None:
             filtered_df = filtered_df[filtered_df['username'] == command_args.user]
