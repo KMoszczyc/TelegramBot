f8303dea4d77c729e8717ed53319e73f651170ed
Kamil Moszczyc <likeyouop@gmail.com>
2024-11-01 16:01:25 +0100

    Fix set_username command v2.

--- src/stats/chat_commands.py
+++ src/stats/chat_commands.py
@@ -269,7 +269,7 @@ class ChatCommands:
             return
 
         text = "Due to missuse of set_username command through various means, it has been indefinitely disabled, until decided otherwise by the Ozjasz team. You can unlock this feature after subscribing to Ozjasz premium, for 5$ monthly. Regards, Ozjasz team."
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
         user_id = update.effective_user.id
         current_username = self.users_df.at[user_id, 'final_username']
