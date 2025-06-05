862c4385c29bbc9a977ec733bfee2a49d0e88928
Kamil Moszczyc <likeyouop@gmail.com>
2025-06-06 01:55:12 +0200

    No chatetl in ozjasz-light.

--- src/main.py
+++ src/main.py
@@ -7,12 +7,4 @@ from src.core.ozjasz_bot import OzjaszBot
 from src.stats.chat_etl import ChatETL
 
 if __name__ == '__main__':
-    # Before running the bot, update the data first.
-    api_handler = ClientAPIHandler()
-    chat_stats = ChatETL(api_handler)
-    if os.path.exists(CHAT_HISTORY_PATH):
-        chat_stats.update(1)
-    else:
-        chat_stats.update(3000)
-
     OzjaszBot()
