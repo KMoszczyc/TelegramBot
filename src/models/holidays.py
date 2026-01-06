8068b884b99d4486b31c6701dcc70859b4b73484
Kamil Moszczyc <likeyouop@gmail.com>
2026-01-06 22:09:41 +0100

    Fix holiday jobs not appearing

--- src/models/holidays.py
+++ src/models/holidays.py
@@ -1,13 +1,20 @@
 import os
 import random
+from datetime import timedelta
 from zoneinfo import ZoneInfo
 import pandas as pd
 from dotenv import load_dotenv
 import telegram
+from sympy.physics.units import minute
 
 from definitions import polish_holidays_df, TIMEZONE, USERS_PATH, CLEANED_CHAT_HISTORY_PATH
 import src.core.utils as core_utils
 import src.stats.utils as stats_utils
+
+pd.options.mode.chained_assignment = None
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
 
 load_dotenv()
 BOT_ID = int(os.getenv('BOT_ID'))
@@ -27,26 +34,31 @@ class Holidays:
 
     def run(self):
         df = self.preprocess_holidays()
-        message_id, message_id_error = stats_utils.get_last_message_id_of_a_user(self.chat_df, BOT_ID)
         for index, row in df.iterrows():
             date, holiday_name, holiday_info = row['date'], row['holiday_name'], row['message']
-            self.job_queue.run_once(callback=lambda context: self.gift_credits_to_all_users(context, message_id, holiday_name, holiday_info), when=date)
+            self.job_queue.run_once(callback=lambda context: self.gift_credits_to_all_users(context, holiday_name, holiday_info), when=date)
 
-    async def gift_credits_to_all_users(self, context, message_id, holiday_name, holiday_info):
+    async def gift_credits_to_all_users(self, context, holiday_name, holiday_info):
         amount = random.randint(500, 3000)
         self.credits.give_credits_to_all(amount)
         message = f"Dziś *{holiday_name}*, w nagrodę wszyscy otrzymują *{amount} kredytów!*\n\n{holiday_info}"
         message = stats_utils.escape_special_characters(message)
-        await context.bot.send_message(chat_id=CHAT_ID, reply_to_message_id=message_id, text=message, message_thread_id=THREAD_ID, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+        await context.bot.send_message(chat_id=CHAT_ID, text=message, message_thread_id=THREAD_ID, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
     def preprocess_holidays(self):
         current_dt = core_utils.get_dt_now()
         raw_holidays_df = polish_holidays_df.copy(deep=True)
         raw_holidays_df['date'] = pd.to_datetime(raw_holidays_df['date'], format='%d-%m-%Y', utc=True).dt.tz_convert(tz=ZoneInfo(TIMEZONE))
 
-        next_year_df = raw_holidays_df.copy(deep=True)
-        next_year_df['date'] = next_year_df['date'].apply(lambda x: x.replace(year=current_dt.year + 1))
-        merged_df = pd.concat([raw_holidays_df, next_year_df], ignore_index=True)
+        years_num = 2  # for how many years should we load holiday jobs - 2 seem enough (current and the next), as Ozjasz is frequently updated, I guess more is unnecessary and could put some overhead?
+        years = [current_dt.year + i for i in range(years_num)]
+        dfs = []
+        for year in years:
+            df = raw_holidays_df.copy(deep=True)
+            df['date'] = df['date'].apply(lambda x: x.replace(year=year, hour=22, minute=7))
+            dfs.append(df)
+
+        merged_df = pd.concat(dfs, ignore_index=True)
         merged_df = merged_df[merged_df['date'] >= current_dt]
 
         return merged_df
